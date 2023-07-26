445cdeaeac1278539919b8a8b02b7b6f7dcb81ae
san <53134807+vishnu9808@users.noreply.github.com>
2023-07-27 01:41:37 +0530

    Update ArmStrong.java

--- ArmStrong.java
+++ ArmStrong.java
@@ -1,1 +1,39 @@
+package com.sample;
+
+public class ArmStrong {
+
+	public static void main(String[] args) {
+	
+		Integer input = 345346346;
+		int length = input.toString().length();
+		char[] data = input.toString().toCharArray();
+		
+		Integer finalValue =0;
+		for(int j = 0; j<input.toString().length(); j++) {
+			System.out.println(data[j]);
+			char val = data[j];
+			Integer intVal = Integer.valueOf(String.valueOf(val));
+			Integer innerFinalVal = 1;
+			for(int i=1;i<=length;i++) {
+				System.out.println("intVal : "+intVal);
+				System.out.println("before multiply innerFinalVal : "+innerFinalVal);
+				innerFinalVal = innerFinalVal*intVal;
+				System.out.println("after multiply innerFinalVal : "+innerFinalVal);
+			}
+			finalValue = finalValue + innerFinalVal;
+			
+			System.out.println("loop finalValue : "+finalValue);
+		}
+		System.out.println(finalValue);
+		System.out.println(input);
+		
+		if(input.equals(finalValue)) {
+			System.out.println("armStrong");
+		}else {
+			System.out.println("not armStrong");
+		}
+	
+	}
+
+}
 
